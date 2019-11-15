7255c63cab478de1e84f920e1c349d9c5ddd186d
manishgdev <manishgdev@gmail.com>
2019-11-15 18:21:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-slider-disable
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-slider-disable)
---
+++ src/app/slider-overview-example.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic slider
+ */
+@Component({
+  selector: 'slider-overview-example',
+  templateUrl: 'slider-overview-example.html',
+  styleUrls: ['slider-overview-example.css'],
+})
+export class SliderOverviewExample {
+
+  isDisabled: any = true;
+
+  onClick(){
+      this.isDisabled = !this.isDisabled;
+  }
+}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
